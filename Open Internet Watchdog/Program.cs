5e31d2197fb82b0a3376910c9923d73ea7424377
Jean-Pierre Hundhausen <github@jp-hundhausen.de>
2018-05-07 20:11:24 +0200

    Not working atm (priority added) WIP

--- Open Internet Watchdog/Program.cs
+++ Open Internet Watchdog/Program.cs
@@ -7,6 +7,8 @@ using System.Xml;
 using System.Xml.Linq;
 using System.Net.NetworkInformation;
 using System.Timers;
+using System.Security.Permissions;
+using System.Linq;
 
 namespace Open_Internet_Watchdog {
     class Program {
@@ -14,27 +16,23 @@ namespace Open_Internet_Watchdog {
         //For hiding console window
         [DllImport("kernel32.dll")]
         static extern IntPtr GetConsoleWindow();
-
         [DllImport("user32.dll")]
         static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
 
-        const int SW_HIDE = 0;
-        const int SW_SHOW = 5;
-
         //file names
-        const string config_file = "config.xml";
-        const string connections_file = "connections.csv";
-        const string log_file = "log.txt";
-        const string offline_log = "offline_log.txt";
+        static string path_for_files = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\Open Internet Watchdog\\";
+        static string config_file = path_for_files + "config.xml";
+        static string connections_file = path_for_files + "connections.csv";
+        static string log_file = path_for_files + "log.txt";
+        static string offline_log = path_for_files + "offline_log.txt";
 
         static bool online = true;
         static DateTime offline_since;
 
         static Timer aTimer;
         static Timer offlineTimer;
-        static List<string> ip = new List<string>();
-        static List<string> domain = new List<string>();
-        
+        static string[][] connections;
+        static string[][] connections_always;
 
         static void Main(string[] args) {
             bool debug, speedtest;
@@ -54,75 +52,104 @@ namespace Open_Internet_Watchdog {
             Console.WriteLine();
             Console.ForegroundColor = ConsoleColor.White;
 
+            if(!Directory.Exists(path_for_files)) {
+                create_folder(path_for_files);
+                }
             if(!File.Exists(config_file)) {
                 debug = true;
-                write_settings();
+                write_config();
                 }
             else {
-                XmlDocument doc = new XmlDocument();
-                doc.Load(config_file);
-                XmlNode node = doc.DocumentElement.SelectSingleNode("/settings/debug");
-                debug = bool.Parse(node.InnerText);
+                debug = bool.Parse(read_config("/settings/show_console"));
                 }
-            if(!debug) {
-                ShowWindow(handle, SW_HIDE);
-                }
-            else {
-                ShowWindow(handle, SW_SHOW);
-                }
+            int sw;
+            sw = (debug) ? 5 : 0; // 5=show, 0=hide
+            ShowWindow(handle, sw); // hides the window, when debug is false
 
-            XmlDocument doc2 = new XmlDocument();
-            doc2.Load(config_file);
-            XmlNode node2 = doc2.DocumentElement.SelectSingleNode("/settings/check_time");
-            connection_time = int.Parse(node2.InnerText)*1000;
+            connection_time = int.Parse(read_config("/settings/check_time")) * 1000;
+
             //reading the connections from the file
-
             using(var sr = new StreamReader(connections_file)) {
+                List<string> ip = new List<string>();
+                List<string> domain = new List<string>();
+                List<string> priority = new List<string>();
+                int always_count = 0;
+                int count = 0;
                 while(!sr.EndOfStream) {
                     var line = sr.ReadLine();
                     var values = line.Split(';');
-
                     domain.Add(values[0]);
                     ip.Add(values[1]);
+                    priority.Add(values[2]);
+                    if(bool.Parse(values[2])) {
+                        always_count++;
+                        }
+                    else {
+                        count++;
+                        }
+                    }
+                //TODO: fix it!
+                connections = new string[count][];
+                connections_always = new string[always_count][];
+                for(int i = 0; i < ip.Count; i++) {
+                    if(bool.Parse(connections[i][2])) {
+                        connections_always[i] = new string[] { domain[i], ip[i], priority[i] };
+                        }
+                    else {
+                        connections[i] = new string[] { domain[i], ip[i], priority[i] };
+                        }
                     }
                 }
+
             write_console_message(2, "Programm started");
             SetTimer(connection_time);
             write_console_message(2, "Pressing enter hides the console (programm still runs)");
+
+
+
+            Console.WriteLine("test");
             Console.ReadKey();
-            ShowWindow(handle, SW_HIDE);
+            if(create_folder(path_for_files)) {
+                Console.WriteLine("true");
+                }
+            else {
+                Console.WriteLine("false");
+                }
+            File.AppendAllText(config_file, "Test");
+            Console.ReadKey();
+            ShowWindow(handle, 0);
             Console.ReadKey();
             }
 
         public static void write_conn_log(bool online) {
             if(online) {
-                    TimeSpan temp = DateTime.Now - offline_since;
-                    File.AppendAllText(offline_log, "[Online] Went online:  " + DateTime.Now +"\n");
-                    write_log("[Online] Went online:  " + DateTime.Now);
-                    int minutes = (temp.Days * 24 * 60)  +(temp.Hours * 60) + (temp.Minutes);
-                    File.AppendAllText(offline_log, "[Online] You was offline for " + minutes + "min " + temp.Seconds +"sec" +"\n");
-                    write_log("[Time] You was offline for " + temp + "min");
-                    offline_since = DateTime.MinValue;
-                    offlineTimer.Stop();
-                    offlineTimer.Dispose();
+                TimeSpan temp = DateTime.Now - offline_since;
+                File.AppendAllText(offline_log, "[Online] Went online:  " + DateTime.Now + "\n");
+                write_log("[Online] Went online:  " + DateTime.Now);
+                int minutes = (temp.Days * 24 * 60) + (temp.Hours * 60) + (temp.Minutes);
+                File.AppendAllText(offline_log, "[Online] You was offline for " + minutes + "min " + temp.Seconds + "sec" + "\n");
+                write_log("[Time] You was offline for " + temp + "min");
+                offline_since = DateTime.MinValue;
+                offlineTimer.Stop();
+                offlineTimer.Dispose();
                 }
             else {
-                    File.AppendAllText(offline_log, "[Offline] Went offline:  " + DateTime.Now + "\n");
-                    write_log("[Offline] Went offline:  " + DateTime.Now);
-                    offline_since = DateTime.Now;
-                    int time;
-                    XmlDocument doc = new XmlDocument();
-                    doc.Load(config_file);
-                    XmlNode node = doc.DocumentElement.SelectSingleNode("/settings/offline_check_time");
-                    time = int.Parse(node.InnerText) * 1000;
-                    SetTimer_offline(time);
+                File.AppendAllText(offline_log, "[Offline] Went offline:  " + DateTime.Now + "\n");
+                write_log("[Offline] Went offline:  " + DateTime.Now);
+                offline_since = DateTime.Now;
+                int time;
+                XmlDocument doc = new XmlDocument();
+                doc.Load(config_file);
+                XmlNode node = doc.DocumentElement.SelectSingleNode("/settings/offline_check_time");
+                time = int.Parse(node.InnerText) * 1000;
+                SetTimer_offline(time);
                 }
             }
 
         public static void write_log(string message) {
-                File.AppendAllText(log_file, DateTime.Now + " - " + message + "\n");
+            File.AppendAllText(log_file, DateTime.Now + " - " + message + "\n");
             }
-        
+
         public static void SetTimer(int timer) {
             aTimer = new Timer(timer);
             aTimer.Elapsed += OnTimedEvent;
@@ -145,21 +172,34 @@ namespace Open_Internet_Watchdog {
             }
 
         public static void timed_event() {
-            for(int i = 0; i < domain.Count; i++) {
-                if(connection_test(domain[i], ip[i])) {
-                    if(!online) {
-                        write_conn_log(true);
-                        }
-                    online = true;
-                    return;
+
+            bool _online = false;
+            for(int i = 0; i < connections_always.Length; i++) {
+                if(connection_test(connections_always[i][0], connections_always[i][1])) {
+                    _online = true;
                     }
                 }
-            if(online) {
-                write_conn_log(false);
+            if(!_online) {
+                Random rnd = new Random();
+                string[][] random_connections = connections.OrderBy(x => rnd.Next()).ToArray();
+                for(int i = 0; i < random_connections.Length; i++) {
+                    if(connection_test(random_connections[i][0], random_connections[i][1])) {
+                        _online = true;
+                        break;
+                        }
+                    }
                 }
-            online = false;
-            write_console_message(4, "You have no connection to the Internet!");
+            if(!_online && online) {
+                write_conn_log(false);
+                online = _online;
+                write_console_message(4, "You have no connection to the Internet!");
+                }
+            else if(_online && !online) {
+                online = _online;
+                write_conn_log(true);
+                }
             }
+
 
         public static bool connection_test(string domain, string ip) {
             string check_data;
@@ -210,9 +250,9 @@ namespace Open_Internet_Watchdog {
                     }
                 }
             catch(Exception) {
-                write_console_message(1,"Ping failed. Maybe the enterd domain is Wrong or IP");
+                write_console_message(1, "Ping failed. Maybe the enterd domain is Wrong or IP");
                 string temp = "Domain: " + domain + " IP: " + ip;
-                write_console_message(1,temp);
+                write_console_message(1, temp);
                 return false;
                 throw;
                 }
@@ -270,7 +310,9 @@ namespace Open_Internet_Watchdog {
                 return true;
                 }
             else {
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("Wrong input!");
+                Console.ForegroundColor = ConsoleColor.White;
                 return bool_check(message);
                 }
             }
@@ -285,12 +327,14 @@ namespace Open_Internet_Watchdog {
             string s = Console.ReadLine();
             int result;
             if(!int.TryParse(s, out result)) {
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("Wrong input!");
+                Console.ForegroundColor = ConsoleColor.White;
                 return int_check(message);
                 }
             return result;
             }
- 
+
         public static List<List<string>> request_ip() {
             bool run = true;
             List<List<string>> results = new List<List<string>>();
@@ -307,10 +351,12 @@ namespace Open_Internet_Watchdog {
                     Console.WriteLine("\nEnter a IP (like this: 0.0.0.0 or 1234:5678:9012:3456::2003):");
                     ip = Console.ReadLine();
                     }
-                if(bool_check("\nIs this right (y/n)?\nDomain: "+ domain +"\nIP: " + ip )) {
+                bool priority = bool_check("\nShould this IP/Domian always get checked?");
+                if(bool_check("\nIs this right (y/n)?\nDomain: " + domain + "\nIP: " + ip)) {
                     results.Add(new List<string>());
                     results[i].Add(domain);
                     results[i].Add(ip);
+                    results[i].Add(priority.ToString());
                     if(bool_check("Do you want to add more? (y/n)")) {
                         i++;
                         }
@@ -322,18 +368,50 @@ namespace Open_Internet_Watchdog {
             return results;
             }
 
-        /// <summary> 
-        /// Writes the settings into a xml file
-        /// </summary>
-        public static void write_settings() {
-            //var
-            bool debug, speedtest;
+        public static bool create_folder(string path) {
+            FileIOPermission f = new FileIOPermission(FileIOPermissionAccess.Read, path);
+            f.AddPathList(FileIOPermissionAccess.Write | FileIOPermissionAccess.Read, path);
+            try {
+                if(!Directory.Exists(path)) {
+                    DirectoryInfo di = Directory.CreateDirectory(path);
+                    }
+                f.Demand();
+                return true;
+                }
+            catch(Exception) {
+                return false;
+                throw;
+                }
+            }
+
+        public static string read_config(string config_name) {
+            try {
+                XmlDocument doc = new XmlDocument();
+                doc.Load(config_file);
+                XmlNode node = doc.DocumentElement.SelectSingleNode(config_name);
+                return node.InnerText;
+                }
+            catch(Exception) {
+                write_console_message(1, "Program paused");
+                write_console_message(1, "The config file is not quite right. Please check the config file or delete it to create a new one");
+                if(bool_check("\nDo you want to create a new config file? (y/n)")) {
+                    write_config();
+                    }
+                else {
+                    write_console_message(2, "Program closed");
+                    Environment.Exit(0);
+                    }
+                throw;
+                }
+            }
+
+        public static void write_config() {
+            bool speedtest, show_console, write_log;
             List<List<string>> ip_check;
             int speedtest_time, check_time, offline_time;
-
-            debug = bool_check("\nShow Console on startup? (y/n)");
+            show_console = bool_check("\nShow Console on startup? (y/n)");
+            write_log = bool_check("\nShould only Errors get written in Log file? (y/n)");
             speedtest = bool_check("\nDo you want to use speedtest? (y/n)");
-            
             if(!speedtest) {
                 speedtest_time = 0;
                 }
@@ -347,15 +425,11 @@ namespace Open_Internet_Watchdog {
 
             new XDocument(
                 new XElement("settings",
-                    new XComment("If set to false, no window will show up on startup of this programm"),
-                    new XElement("debug", debug),
-                    new XComment("Enabling a speetest. Note: this speedtest is not reliable, because you might use your bandwith with downloads or similar."),
+                    new XElement("show_console", show_console),
+                    new XElement("write_only_errors", write_log),
                     new XElement("speedtest", speedtest),
-                    new XComment("How often a Speedtest should run. Note: don't pick a too short time"),
                     new XElement("speed_time", speedtest_time),
-                    new XComment("How often the programm checks if a connection to the internet is avaible"),
                     new XElement("check_time", check_time),
-                    new XComment("How often the programm checks if a connection to the internet is avaible, when you are offilne"),
                     new XElement("offline_check_time", offline_time)
                 )
             )
@@ -363,9 +437,11 @@ namespace Open_Internet_Watchdog {
             using(StreamWriter sw =
              new StreamWriter(connections_file)) {
                 for(int i = 0; i < ip_check.Count; i++) {
-                    sw.WriteLine(ip_check[i][0] + ";" + ip_check[i][1]);
+                    sw.WriteLine(ip_check[i][0] + ";" + ip_check[i][1] + ";" + ip_check[i][2]);
                     }
                 }
+
             }
+
         }
     }
